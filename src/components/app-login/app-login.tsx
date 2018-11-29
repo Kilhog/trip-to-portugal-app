9d490c8c6a11ce89bf91ba6db98bc4588654236a
Kévin LE QUILLEC <kqc@tetra-info.com>
2018-11-29 13:41:46 +0100

    🚧 Login Facebook

--- src/components/app-login/app-login.tsx
+++ src/components/app-login/app-login.tsx
@@ -1,4 +1,7 @@
 import { Component } from '@stencil/core';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 
 @Component({
   tag: 'app-login',
@@ -15,14 +18,39 @@ export class AppLogin {
       </ion-header>,
 
       <ion-content padding>
-        <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire
-          apps with web components using Stencil and ionic/core! Check out the
-          README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
+        <ion-button onClick={() => {
+          console.log(" click ");
+          var provider = new firebase.auth.FacebookAuthProvider();
+          provider.addScope('email');
+          firebase.auth().languageCode = 'fr_FR';
+          provider.setCustomParameters({
+            'display': 'popup'
+          });
+          console.log(" signInWithPopup ");
 
-        <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
+          firebase.auth().signInWithPopup(provider).then(function(result) {
+            console.log(' -- GOOD ');
+            console.log(result);
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            // var token = result.credential.accessToken;
+            // The signed-in user info.
+            // var user = result.user;
+            // ...
+          }).catch(function(error) {
+            console.log(' -- ERROR ');
+            console.log(error);
+
+            // Handle Errors here.
+            // var errorCode = error.code;
+            // var errorMessage = error.message;
+            // The email of the user's account used.
+            // var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            // var credential = error.credential;
+            // ...
+          });
+
+        }} expand="block">Facebook</ion-button>
       </ion-content>
     ];
   }
